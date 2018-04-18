12b4acb06cfb39000b174a13a38a6529c80ca348
Cole Challand <ctchalland@gmail.com>
2018-04-18 02:35:26 -0500

    Completed 10-6 exercise

---
+++ 10-Ch_Character&C-Strings&More/10-7_strcmp-Function.cpp
@@ -0,0 +1,13 @@
+// This program test two C-strings for equality using the strcmp function.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  // Two arrays for two strings.
+  const int LENGTH = 40;
+  
+
+  return 0;
+}
